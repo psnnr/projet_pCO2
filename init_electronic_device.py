1dc247b6ce5d6b12f30db561727b80e9b5cc5e3b
Paul Sonnier <paul.sonnier@hotmail.fr>
2019-05-21 16:33:27 +0200

    attributes

--- init_electronic_device.py
+++ init_electronic_device.py
@@ -1,16 +1,18 @@
 import serial
 
 def init_time(cls):
-    u = cls.get_t_mesure_atm_air()
-    print(u)
+    w = cls.get_t_mesure_atm_air()
+    u = int(w)
 
 def init_elec_device(cls):
 
+    a = cls.get_gps_bytesize()
+    print(a)
+    print(type(str(a)))
 
-    ser_GPS = serial.Serial(cls.gps["serial_port"],
-    baudrate=cls.gps["baudrate"],
-    parity=cls.gps["parity"],
-    stopbits=cls.gps["stops_bits"],
-    bytesize= serial.)
-
-    print(cls.gps["baudrate"])
+    ser_GPS = serial.Serial(str(cls.get_gps_serial_port),
+    baudrate=str(cls.get_gps_baudrate),
+    parity=str(cls.get_gps_parity),
+    stopbits=str(cls.get_gps_stops_bits),
+    bytesize= serial.EIGHTBITS)
+    #get_gps_bytesize)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import init_electronic_device as IED
 #    print(params)
 
 def load_params(fname = "configuration.toml",param = ""):
+    ### display toml file
     params = {}
     with open(fname, 'r', encoding='utf-8') as fp:
         params.update(toml.load(fp))
@@ -80,8 +81,6 @@ class config_time():
     def get_t_mesure_sea_water(self):
         return self.__t_mesure_sea_water
 
-
-
 class config_elec_periph():
 
     ### contain all inforation about electronical device connexion
@@ -97,50 +96,197 @@ class config_elec_periph():
         thermosalinometer = parsed_toml['thermosalinometer']
 
 
-        self.periph = periph
-        self.ir_analyzer = ir_analyzer
-        self.gps = gps
-        self.temperature_sensor = temperature_sensor
-        self.oxygen_sensor = oxygen_sensor
-        self.interface_box_chip = interface_box_chip
-        self.thermosalinometer = thermosalinometer
+        ### peripheral attributes (on/off)
 
-    def get_periph(self):
-        return self.periph
+        self.__periph_IR_analyzer = periph["IR_analyzer"]
+        self.__periph_GPS = periph["GPS"]
+        self.__periph_temperature_sensor = periph["temperature_sensor"]
+        self.__periph_oxygen_sensor = periph["oxygen_sensor"]
+        self.__periph_interface_box_chip = periph["interface_box_chip"]
+        self.__periph_barometer_atmospheric = periph["barometer_atmospheric"]
+        self.__barometer_equi = periph["barometer_equi"]
+        self.__thermosalinometer = periph["thermosalinometer"]
 
-    def get_ir_analyzer(self):
-        return self.ir_analyzer
+        ### ir analyzer configurations attributes
 
-    def get_gps_serial_port():
-        return self.gps["serial_port"]
+        self.__ir_analyzer_active = ir_analyzer["active"]
+        self.__ir_analyzer_serial_port = ir_analyzer["serial_port"]
+        self.__ir_analyzer_baudrate = ir_analyzer["baudrate"]
+        self.__ir_analyzer_data_bits = ir_analyzer["data_bits"]
+        self.__ir_analyzer_stops_bits = ir_analyzer["stops_bits"]
+        self.__ir_analyzer_parity = ir_analyzer["parity"]
 
-    def get_gps_baudrate():
-        return self.gps["baudrate"]
+        ### gps configurations attributes
 
-    def get_gps_parity():
-        return self.gps["parity"]
+        self.__gps_active = gps["active"]
+        self.__gps_serial_port = gps["serial_port"]
+        self.__gps_baudrate = gps["baudrate"]
+        self.__gps_data_bits = gps["data_bits"]
+        self.__gps_stops_bits = gps["stops_bits"]
+        self.__gps_parity = gps["parity"]
 
-    def get_gps_stops_bits():
-        return self.gps["stops_bits"]
+        ### temperature sensor configurations attributes
+
+        self.__temperature_sensor_active = temperature_sensor["active"]
+        self.__temperature_sensor_serial_port = temperature_sensor["serial_port"]
+        self.__temperature_sensor_baudrate = temperature_sensor["baudrate"]
+        self.__temperature_sensor_data_bits = temperature_sensor["data_bits"]
+        self.__temperature_sensor_stops_bits = temperature_sensor["stops_bits"]
+        self.__temperature_sensor_parity = temperature_sensor["parity"]
+        
+        ### oxygen sensor configurations attributes
+
+        self.__oxygen_sensor_active = oxygen_sensor["active"]
+        self.__oxygen_sensor_serial_port = oxygen_sensor["serial_port"]
+        self.__oxygen_sensor_baudrate = oxygen_sensor["baudrate"]
+        self.__oxygen_sensor_data_bits = oxygen_sensor["data_bits"]
+        self.__oxygen_sensor_stops_bits = oxygen_sensor["stops_bits"]
+        self.__oxygen_sensor_parity = oxygen_sensor["parity"]
+
+        ### interface box chip configurations attributes
+
+        self.__interface_box_chip_active = interface_box_chip["active"]
+        self.__interface_box_chip_serial_port = interface_box_chip["serial_port"]
+        self.__interface_box_chip_baudrate = interface_box_chip["baudrate"]
+        self.__interface_box_chip_data_bits = interface_box_chip["data_bits"]
+        self.__interface_box_chip_stops_bits = interface_box_chip["stops_bits"]
+        self.__interface_box_chip_parity = interface_box_chip["parity"]
+
+        ### thermosalinometer configurations attributes
+
+        self.__thermosalinometer_active = thermosalinometer["active"]
+        self.__thermosalinometer_serial_port = thermosalinometer["serial_port"]
+        self.__thermosalinometer_baudrate = thermosalinometer["baudrate"]
+        self.__thermosalinometer_data_bits = thermosalinometer["data_bits"]
+        self.__thermosalinometer_stops_bits = thermosalinometer["stops_bits"]
+        self.__thermosalinometer_parity = thermosalinometer["parity"]
+
+        ### getter peripheral
+
+    def get_periph_IR_analyzer(self):
+        return self.__periph_IR_analyzer
+
+    def get_periph_GPS(self):
+        return self.__periph_GPS
+
+    def get_periph_temperature_sensor(self):
+        return self.__periph_temperature_sensor
+
+    def get_periph_oxygen_sensor(self):
+        return self.__periph_oxygen_sensor
+
+    def get_periph_interface_box_chip(self):
+        return self.__periph_interface_box_chip
+
+    def get_periph_barometer_atmospheric(self):
+        return self.__periph_barometer_atmospheric
+
+    def get_periph_barometer_equi(self):
+        return self.__periph_barometer_equi
+
+    def get_periph_thermosalinometer(self):
+        return self.__periph_thermosalinometer
+
+        ### getter ir analyzer
+
+    def get_IR_analyzer_active(self):
+        return self.__IR_analyzer_active
+
+    def get_IR_analyzer_serial_port(self):
+        return self.__IR_analyzer_serial_port
+
+    def get_IR_analyzer_baudrate(self):
+        return self.__IR_analyzer_baudrate
+
+    def get_IR_analyzer_stops_bits(self):
+        return self.__IR_analyzer_stops_bits
+
+    def get_IR_analyzer_parity(self):
+        return self.__IR_analyzer_parity
+
+    def get_IR_analyser_bytesize(self):
+        return self.__gps_IR_analyser
+
+    ### getter GPS
+
+    def get_gps_active(self):
+        return self.__gps_active
+
+    def get_gps_serial_port(self):
+        return self.__gps_serial_port
+
+    def get_gps_baudrate(self):
+        return self.__gps_baudrate
+
+    def get_gps_stops_bits(self):
+        return self.__gps_stops_bits
+
+    def get_gps_parity(self):
+        return self.__gps_parity
+
+    def get_gps_bytesize(self):
+        return self.__gps_data_bits
+
+    ### getter oxygen sensor
+
+    def get_oxygen_sensor_active(self):
+        return self.__oxygen_sensor_active
+
+    def get_oxygen_sensor_serial_port(self):
+        return self.__oxygen_sensor_serial_port
+
+    def get_oxygen_sensor_baudrate(self):
+        return self.__oxygen_sensor_baudrate
+
+    def get_oxygen_sensor_stops_bits(self):
+        return self.__oxygen_sensor_stops_bits
+
+    def get_oxygen_sensor_parity(self):
+        return self.__oxygen_sensor_parity
     
-    def get_gps_bytesize():
-        return self.gps["bytesize"]
+    def get_oxygen_sensor_bytesize(self):
+        return self.__oxygen_sensor_data_bits
 
+    ### getter interface box ship
 
+    def get_interface_box_chip_active(self):
+        return self.__interface_box_chip_active
 
+    def get_interface_box_chip_serial_port(self):
+        return self.__interface_box_chip_serial_port
 
+    def get_interface_box_chip_baudrate(self):
+        return self.__interface_box_chip_baudrate
 
-def init_elec_device(cls):
+    def get_interface_box_chip_stops_bits(self):
+        return self.__interface_box_chip_stops_bits
 
-    #ser_GPS = serial.Serial(str(cls.get_gps_serial_port),
-    #baudrate=str(cls.get_gps_baudrate),
-    #parity=str(cls.get_gps_parity),
-    #stopbits=str(cls.get_gps_stops_bits),
-    #bytesize= int(str(cls.get_gps_bytesize)))
+    def get_interface_box_chip_parity(self):
+        return self.__interface_box_chip_parity
+    
+    def get_interface_box_chip_bytesize(self):
+        return self.__interface_box_chip_data_bits
 
-    a = cls.get_gps_baudrate
-    print(a)
+    ### getter thermosalinometer
 
+    def get_thermosalinometer_active(self):
+        return self.__thermosalinometer_active
+
+    def get_thermosalinometer_serial_port(self):
+        return self.__thermosalinometer_serial_port
+
+    def get_thermosalinometer_baudrate(self):
+        return self.__thermosalinometer_baudrate
+
+    def get_thermosalinometer_stops_bits(self):
+        return self.__thermosalinometer_stops_bits
+
+    def get_thermosalinometer_parity(self):
+        return self.__thermosalinometer_parity
+
+    def get_thermosalinometer_bytesize(self):
+        return self.__thermosalinometer_data_bits
+    
 ### class intances
 time = config_time()
 
@@ -148,9 +294,8 @@ connec = config_elec_periph()
 
 IED.init_time(time)
 
-#info = connec.get_periph()
-#print(info)
+info = connec.get_periph_IR_analyzer()
+print(info)
+print(type(info))
 
-init_elec_device(connec)
-
-#init_elec_device(connec)
+IED.init_elec_device(connec)
